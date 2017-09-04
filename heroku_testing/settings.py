d3337389f0d7a9f0f1a02e248a23c846ea200bb6
smithmayowa20 <smithmayowa20@yahoo.com>
2017-09-04 15:28:27 +0100

    changed the path in staticfiles_dir

--- heroku_testing/settings.py
+++ heroku_testing/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
