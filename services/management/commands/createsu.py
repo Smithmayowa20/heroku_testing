baea72911a9bb7915a030260066b2562f0dd31c6
smithmayowa20 <smithmayowa20@yahoo.com>
2017-10-26 18:19:35 +0100

    changes in settings

--- services/management/commands/createsu.py
+++ services/management/commands/createsu.py
@@ -1,9 +1,13 @@
+import os
+from decouple import config, Csv
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
-
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        username = config['SUPER_USER_NAME']
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username,
+                                          config['SUPER_USER_EMAIL'],
+                                          config['SUPER_USER_PASSWORD'])
